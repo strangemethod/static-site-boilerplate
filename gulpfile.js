5b0a81920bac73b7879eb05c9b26c13128d66ef8
Andrew Bowles <Andrew.Bowles@rga.com>
2017-03-16 11:44:37 -0700

    add handlebars to watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,97 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    data = require('gulp-data'),
-    handlebars = require('gulp-compile-handlebars'),
-    rename = require('gulp-rename'),
-    notify = require('gulp-notify'),
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    connect = require('gulp-connect'),
-    plumber = require('gulp-plumber'),
-    autoprefixer = require('gulp-autoprefixer'),
-    server = lr(),
-    path = require('path');
+	gutil = require('gulp-util'),
+	sass = require('gulp-sass'),
+	data = require('gulp-data'),
+	handlebars = require('gulp-compile-handlebars'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	livereload = require('gulp-livereload'),
+	lr = require('tiny-lr'),
+	connect = require('gulp-connect'),
+	plumber = require('gulp-plumber'),
+	autoprefixer = require('gulp-autoprefixer'),
+	server = lr(),
+	path = require('path');
 
 var paths = {
-  templates: './templates',
-  sass: './sass',
-  assets: './assets',
-  data: './data'
+	templates: './templates',
+	sass: './sass',
+	assets: './assets',
+	data: './data'
 };
 
 
 gulp.task('handlebars', function() {
 
-    var templateData = {},
+	var templateData = {},
 
-    options = {
-        ignorePartials: true,
-        batch : ['./partials']
-    }
- 
-    return gulp.src(path.join(paths.templates, '*.handlebars'))
-        .pipe(data(function(file) {
-          return require('./data/app.json');
-        }))
-        .pipe(handlebars(templateData, options))
-        .pipe(rename({
-          extname: ""
-         }))
-        .pipe(rename({
-          extname: ".html"
-         }))
-        .pipe(gulp.dest('./dist'));
+	options = {
+			ignorePartials: true,
+			batch : ['./partials']
+	}
+
+	return gulp.src(path.join(paths.templates, '*.handlebars'))
+			.pipe(data(function(file) {
+				return require('./data/app.json');
+			}))
+			.pipe(handlebars(templateData, options))
+			.pipe(rename({
+				extname: ""
+			 }))
+			.pipe(rename({
+				extname: ".html"
+			 }))
+			.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('sass', function() {
-  return gulp.src(path.join(paths.sass, '*.scss'))
-    .pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true}))
-    .pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'SASS compiled!' }));
+	return gulp.src(path.join(paths.sass, '*.scss'))
+		.pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true}))
+		.pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(livereload(server))
+		.pipe(notify({ message: 'SASS compiled!' }));
 });
 
 gulp.task('static-assets', function() {
-  gulp.src(path.join(paths.assets, '*'))
-    .pipe(gulp.dest('./dist/assets'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Static Assets copied' }));
-  gulp.src('node_modules/normalize.css/normalize.css')
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Normalize copied' }));
+	gulp.src(path.join(paths.assets, '*'))
+		.pipe(gulp.dest('./dist/assets'))
+		.pipe(livereload(server))
+		.pipe(notify({ message: 'Static Assets copied' }));
+	gulp.src('node_modules/normalize.css/normalize.css')
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(livereload(server))
+		.pipe(notify({ message: 'Normalize copied' }));
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    port: 1337,
-    root: ['dist'],
-    livereload: false
-  });
+	connect.server({
+		port: 1337,
+		root: ['dist'],
+		livereload: false
+	});
 });
 
 function watchStuff(task) {
-  // Listen on port 35729
-  server.listen(35729, function (err) {
-    if (err) {
-      return console.error(err) 
-    };
+	// Listen on port 35729
+	server.listen(35729, function (err) {
+		if (err) {
+			return console.error(err) 
+		};
 
-    //Watch task for sass
-    gulp.watch(path.join(paths.sass, '**/*.scss'), [task]);
+		//Watch task for sass
+		gulp.watch(path.join(paths.sass, '**/*.scss'), [task]);
 
-    // watch task for gulp-includes
-    gulp.watch(path.join(paths.templates, '**/*.handlebars'), ['handlebars']);
+		// watch task for gulp-includes
+		gulp.watch(path.join(paths.templates, '**/*.handlebars'), ['handlebars']);
 
-  });
+	});
 }
 
 
 gulp.task('watch', function() {
- watchStuff('sass');
+	watchStuff('sass');
+	watchStuff('handlebars');
 });
 
 
