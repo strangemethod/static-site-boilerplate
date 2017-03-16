df21d8b5add068e05d79afb65856a04fe28d57e2
Andrew Bowles <Andrew.Bowles@rga.com>
2017-03-16 15:25:08 -0700

    implement components

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	data = require('gulp-data'),
-	handlebars = require('gulp-compile-handlebars'),
+	hb = require('gulp-hb'),
 	rename = require('gulp-rename'),
 	// notify = require('gulp-notify'),
 	livereload = require('gulp-livereload'),
@@ -13,39 +13,54 @@ var gulp = require('gulp'),
 	plumber = require('gulp-plumber'),
 	autoprefixer = require('gulp-autoprefixer'),
 	server = lr(),
+	merge = require('merge-stream'),
 	path = require('path');
 
 
 var paths = {
 	templates: './src/templates/',
 	partials: './src/partials/',
+	components: './src/components/',
 	sass: './src/sass/',
-	js: './src/js/',
+	scripts: './src/js/',
 	data: './src/data/'
 };
 
 
-gulp.task('handlebars', function() {
+gulp.task('handlebars', function () {
+    var hbStream = hb()
+        .partials(path.join(paths.partials, '*.hbs'))
+        .partials(path.join(paths.components, '**/*.hbs'))
+        .data(path.join(paths.data, '*.json'));
 
-	var templateData = {},
-	options = {
-			ignorePartials: true,
-			batch : [paths.partials]
-	}
+    return gulp
+        .src(path.join(paths.templates, '*.hbs'))
+        .pipe(hbStream)
+		.pipe(rename({
+			extname: ""
+		 }))
+		.pipe(rename({
+			extname: ".html"
+		 }))
+        .pipe(gulp.dest('./public'))
+		.pipe(livereload(server));
+});
 
-	return gulp.src(path.join(paths.templates, '*.handlebars'))
-			.pipe(data(function(file) {
-				return require(paths.data + 'data.json');
-			}))
-			.pipe(handlebars(templateData, options))
-			.pipe(rename({
-				extname: ""
-			 }))
-			.pipe(rename({
-				extname: ".html"
-			 }))
-			.pipe(gulp.dest('./public'))
-			.pipe(livereload(server));
+gulp.task('components', function() {
+	var compileSass = gulp.src(path.join(paths.components, '**/*.scss'))
+		.pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true}))
+		.pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
+		.pipe(concat('components.css'))
+		.pipe(gulp.dest('./public/css'))
+		.pipe(livereload(server));
+
+	var compileJs = gulp.src(path.join(paths.components, '**/*.js'))
+	    .pipe(concat('components.js'))
+	    .pipe(uglify())
+	    .pipe(gulp.dest('./public/js'))
+		.pipe(livereload(server));
+
+	return merge(compileSass, compileJs);
 });
 
 
@@ -58,10 +73,9 @@ gulp.task('sass', function() {
 		// .pipe(notify({ message: 'SASS compiled!' }));
 });
 
-
 gulp.task('scripts', function() {
-  return gulp.src(path.join(paths.js, '*.js'))
-    .pipe(concat('all.js'))
+  return gulp.src(path.join(paths.scripts, '*.js'))
+    .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./public/js'))
 	.pipe(livereload(server));
@@ -85,21 +99,18 @@ gulp.task('watch', function() {
 			return console.error(err) 
 		};
 
-		// watch task for gulp-includes
-		gulp.watch(path.join(paths.partials, '**/*.handlebars'), ['handlebars']);
-		gulp.watch(path.join(paths.templates, '**/*.handlebars'), ['handlebars']);
-
-		//Watch task for sass
+		gulp.watch(path.join(paths.partials, '**/*.hbs'), ['handlebars']);
+		gulp.watch(path.join(paths.templates, '**/*.hbs'), ['handlebars']);
+		gulp.watch(path.join(paths.components, '**/*.hbs'), ['handlebars']);
+		gulp.watch(path.join(paths.components, '**/*.{scss,js}'), ['components']);
 		gulp.watch(path.join(paths.sass, '**/*.scss'), ['sass']);
-
-		//Watch task for js
-		gulp.watch(path.join(paths.js, '**/*.js'), ['scripts']);
+		gulp.watch(path.join(paths.scripts, '**/*.js'), ['scripts']);
 
 	});
 
 });
 
 
-gulp.task('default', ['handlebars', 'sass', 'scripts', 'connect', 'watch'], function() {
+gulp.task('default', ['handlebars', 'components', 'sass', 'scripts', 'connect', 'watch'], function() {
 
 });
---
+++ public/js/components.js
@@ -0,0 +1,1 @@
+console.log("init carousel"),console.log("init hero");
