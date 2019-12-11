c5ebdea5a2853e327ece9e8ad7627b904e05c555
Andrew Bowles <Andrew.Bowles@rga.com>
2019-12-11 10:45:00 -0800

    Add layouts helper

--- gulp/handlebars.js
+++ gulp/handlebars.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var data = require('gulp-data');
 var hb = require('gulp-hb');
 var rename = require('gulp-rename');
+var layouts = require('handlebars-layouts');
 var livereload = require('gulp-livereload');
 var lr = require('tiny-lr');
 var server = lr();
 var path = require('path');
 var CONFIG_ = require('../config');
-
 
 /*
  * Compile handlebars templates
@@ -17,7 +17,8 @@ gulp.task('handlebars', function () {
   var hbStream = hb()
     .partials(path.join(CONFIG_.paths.partials, '*.hbs'))
     .partials(path.join(CONFIG_.paths.components, '**/*.hbs'))
-    .data(path.join(CONFIG_.paths.data, '*.json'));
+    .data(path.join(CONFIG_.paths.data, '*.json'))
+    .helpers(layouts);
 
     return gulp
       .src(path.join(CONFIG_.paths.templates, '*.hbs'))
